f8deef61c91b8977e21d3774c63b17f3123973a4
ElMoik <miguel.moreno@cetis107.edu.mx>
2022-11-19 12:55:47 -0700

    grupos

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,31 @@
 <?php
-use App\Http\Controllers\AlumnoController;
-use App\Http\Controllers\EspecialidadController;
-use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get('/', [HomeController::class, 'Home']);
-//Alumnos
-Route::get('/alumnos', [AlumnoController::class, 'consultar']);
-Route::get('/alumno/registrar', [AlumnoController::class, 'registrar']);
-Route::post('/alumno/guardar', [AlumnoController::class, 'guardar']);
-Route::get('/alumno/eliminar/{id}', [AlumnoController::class, 'eliminar']);
-Route::get('/alumno/editar/{id}', [AlumnoController::class, 'editar']);
-Route::post('/alumno/actualizar/{id}', [AlumnoController::class, 'actualizar']);
-//Especialidades
-Route::get('/especialidades', [EspecialidadController::class, 'consultar']);
-Route::get('/especialidad/registrar', [EspecialidadController::class, 'registrar']);
-Route::post('/especialidad/guardar', [EspecialidadController::class, 'guardar']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
